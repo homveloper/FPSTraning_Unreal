e6b803cb414337045b5bd18fcf0f0b2553c28194
homveloper <bum4496@naver.com>
2022-03-22 00:07:33 +0900

    초기화 : 프로젝트 설정 및 ALS 플러그인

---
+++ Source/FPSTraning/FPSTraningGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FPSTraningGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FPSTRANING_API AFPSTraningGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
